fc457fbdf6ec887a0c5dec7d5cf47c804a5004c8
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2022-09-21 19:16:18 -0300

    corrigindo o erro de código

--- questão 6.py
+++ questão 6.py
@@ -4,37 +4,16 @@ numero_3 = int(input("Insira um número: "))
 
 reserva = 0
 
-if numero_2 > numero and numero_2 > numero_3:
+if (numero_2 > numero):
     reserva = numero
     numero = numero_2
     numero_2 = reserva
-    if numero_3 < numero_2:
-        reserva = numero_2
-        numero_2 = numero_3
-        numero_3 = reserva
-    else:
-        numero_2 = numero_2
-        numero_3 = numero_3
-if numero_3 > numero and numero_3 > numero_2:
-    reserva = numero
-    numero = numero_3
+if (numero_3 > numero_2):
+    reserva = numero_2
+    numero_2 = numero_3
     numero_3 = reserva
-    print(numero, numero_2, numero_3)
-    if numero_2 < numero_3:
-        reserva = numero_2
-        numero_2 = numero_3
-        numero_3 = reserva
-    else:
-        numero_2 = numero_2
-        numero_3 = numero_3
-else:
-    numero = numero
-    if numero_3 < numero_2:
-        numero_2 = numero_2
-        numero_3 = numero_3  
-    else:
-        reserva = numero_2
-        numero_2 = numero_3
-        numero_3 = reserva
-
+if (numero_2 > numero):
+    reserva = numero
+    numero = numero_2
+    numero_2 = reserva
 print(numero, numero_2, numero_3)
