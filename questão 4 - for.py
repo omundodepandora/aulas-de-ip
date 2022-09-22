987b60a428f1e97e5796853fc98351997986ab90
Talita Oliveira <82682354+talitagiovanna@users.noreply.github.com>
2022-09-21 21:50:01 -0300

    exercícios de for

---
+++ questão 4 - for.py
@@ -0,0 +1,3 @@
+numero = int(input("Digite um número maior que 0: "))
+for i in range(numero, 0, -1):
+    print(i, "patinhos foram passear\nAlém das montanhas para brincar\nA mamãe gritou: Quá, quá, quá, quá\nMas só", i-1,"patinhos voltaram de lá.")
